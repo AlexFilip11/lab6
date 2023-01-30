8fd8ea56b9688c28614744f522024063b23de565
Alex Filip <alexfilippaul@gmail.com>
2023-01-30 22:04:45 +0200

    Add project files.

---
+++ lab6/Dulap.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab6
+{
+    class Dulap
+    {
+
+        private int lungime;
+        private int latime;
+        private int inaltime;
+        
+        public void AfisareDulap(Dulap myDulap)
+        {
+            Console.WriteLine( $"Dulapul are lungimea de {lungime}cm, latimea de {latime}cm si inaltimea de {inaltime}cm");
+        }
+        public void CitesteDulap(Dulap myDulap)
+        {
+            Console.WriteLine("Lungime: ");
+            lungime = int.Parse(Console.ReadLine());
+            Console.WriteLine("Latime: ");
+            latime = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inaltime: ");
+            inaltime = int.Parse(Console.ReadLine());
+        }
+        public int GetLungime()
+        {
+            return lungime;
+        }
+        public int GetLatime()
+        {
+            return latime;
+        }
+        public int GetInaltime()
+        {
+            return inaltime;
+        }
+    }
+    class Autoturism
+    {
+        public string marca;
+        public string nrInmatriculare;
+        private int capacitateCilindrica;
+        public void AfisareAutoturism(Autoturism myAuto)
+        {
+            Console.WriteLine($"Autoturismul este marca {marca} cu numarul de inmatriculare {nrInmatriculare} si capacitatea cilindrica de {capacitateCilindrica}");
+        }
+        public void CitesteAuto(Autoturism myAuto)
+        {
+            Console.WriteLine("Marca: ");
+            marca = Console.ReadLine();
+            Console.WriteLine("Numarul de inmatriculare: ");
+            nrInmatriculare = Console.ReadLine();
+            Console.WriteLine("Capacitatea cilindrica: ");
+            capacitateCilindrica = int.Parse(Console.ReadLine());
+        }
+        public int GetCapacitateaCilindrica()
+        {
+            return capacitateCilindrica;
+        }
+
+    }
+}
---
+++ lab6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex1();
+            Ex2();
+        }
+
+        static void Ex1()
+        {
+            //Un dulap are trei caracteristici: lungime, latime, inaltime.Dulapul va avea o metoda care va calcula volumul acestuia.
+            //Folositi o clasa care va modela un dulap.
+            // Clasa va avea trei campuri private intregi: lungime, latime, inaltime.
+            // Cele trei campuri vor fi initializate cu ajutorul constructorului
+            // Scrieti o metoda care va calcula volumul dulapului pe baza celor trei campuri.
+            //Instantiati un dulap in functia Main, apelati metoda de calcul a volumului si afisati rezultatul.
+            Dulap dulap = new Dulap();
+            dulap.CitesteDulap(dulap);
+            dulap.AfisareDulap(dulap);
+            Console.WriteLine($"Volumul dulapului este {CalzuleazaVolumDulap(dulap)}");
+        }
+
+        static int CalzuleazaVolumDulap(Dulap myDulap)
+        {
+            int result = myDulap.GetLungime() * myDulap.GetLatime() * myDulap.GetInaltime();
+            return result;
+        }
+        
+        static void Ex2()
+        {
+            //Scrieti un program care va modela un autoturism. Un autoturism va avea o marca(string), un numar de inmatriculare(string), precum si o capacitate cilindrica(int).
+            //Autoturismul va avea:
+            // Campurile corespunzatoare caracteristicilor autoturismului.
+            // Un constructor care va initializa campurile acestuia
+            // O metoda “Tiparire” care va afisa pe consola descrierea acestuia sub forma: “marca, numarDeInmatriculare, capacitateCilindrica”.
+            //Folositi: clase, campuri, metode, modificatorii de acces adecvati, comentarii xml pentru metodele publice.
+            //Creeati doua obiecte de tipul autoturism in functia “Main” si apelati-le metodele “Tipareste”
+            Autoturism masina = new Autoturism();
+            masina.CitesteAuto(masina);
+            masina.AfisareAutoturism(masina);
+            Autoturism autobus = new Autoturism();
+            autobus.CitesteAuto(autobus);
+            autobus.AfisareAutoturism(autobus);
+        }
+    }
+}
